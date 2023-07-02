1403a8ded8b83cf5ef26c7952367bbcf0310f304
Hatim Daoudi <hey@hatimdaoudi.com>
2023-07-02 04:21:36 +0200

    hadik

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: "Hati  Da" });
+  res.status(200).json({ name: "Hat  Da" });
 }
